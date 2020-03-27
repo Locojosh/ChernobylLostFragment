ce1ec4bb9f510da4f39e1388c84e45085a02f31b
Josh <joshuasteele.student@gmail.com>
2020-03-27 17:13:32 -0400

    Programacion- Interfaz_Control> Estructura de canvas de salir y cargar partida

---
+++ Assets/Scripts/Menu_De_Inicio_Control.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_De_Inicio_Control : MonoBehaviour
+{
+#region ATRIBUTOS
+
+
+#endregion
+
+#region METODOS POR DEFECTO
+
+    private void Awake() 
+    {
+        
+    }
+    private void Start()
+    {
+        gameObject.SetActive(true);
+    }
+    
+    private void Update()
+    {
+        
+    }
+    #endregion
+
+    #region METODOS CUSTOM
+    public void OnClick_NuevaPartida()
+    {
+
+    }
+    #endregion
+}
