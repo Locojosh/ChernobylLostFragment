7fd47d5eb22c182c6cbd81d5eb5560a18c4f385c
Josh <joshuasteele.student@gmail.com>
2020-04-04 23:09:10 -0400

    Programacion- Jugador_Ataque: Proceso en el script

---
+++ Assets/Scripts/Enemigo_Control.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo_Control : MonoBehaviour
+{
+    public int vida = 100; //hacer private 
+    public int Vida { get{ return vida;} }
+
+#region Metodos Por Defecto
+    public void QuitarVida(int valor)
+    {
+        vida -= valor;
+    }
+
+#endregion
+}
---
+++ Assets/Scripts/Jugador_Ataque.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador_Ataque : MonoBehaviour
+{
+    public int porcentajeDañoBarreta = 25;
+    public int porcentajeDañoArmaScientifica = 15;
+    public int porcentajeDañoExplosivo = 50;
+
+    public GameObject cuboCerca;
+    private BoxCollider coliderA;
+
+    private void Awake() 
+    {
+        coliderA = transform.Find("Ataque_Collider").gameObject.GetComponent<BoxCollider>();
+        
+    }
+    private void OnTriggerStay(Collider other) //BARRETA
+    {
+        if(other.tag == "enemigo")
+        if(Input.GetButtonDown("Arma"))
+        {
+            DañarEnemigo(other.gameObject, porcentajeDañoBarreta); 
+        }         
+    }
+    private void UsarArmaScientifica()
+    {
+
+    }
+    private void TirarExplosivo()
+    {
+
+    }
+
+    private void DañarEnemigo(GameObject enemigo, int daño)
+    {
+        enemigo.GetComponent<Enemigo_Control>().QuitarVida(daño);
+        //Animacion     
+        Animator anim = enemigo.GetComponent<Animator>();  
+        if()
+    }
+}
