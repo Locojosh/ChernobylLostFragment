0d5579dad4b49012878d5303593cf0689ba9e4d2
Josh <joshuasteele.student@gmail.com>
2020-04-06 21:57:40 -0400

    Programacion- Jugador_Ataque> Proceso: Cambiar arma actualmente seleccionada, input CambiarArma

--- Assets/Scripts/Jugador_Ataque.cs
+++ Assets/Scripts/Jugador_Ataque.cs
@@ -1,25 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Jugador_Ataque : MonoBehaviour
 {
+    //Generales
     public int porcentajeDañoBarreta = 25;
     public int porcentajeDañoArmaScientifica = 15;
     public int porcentajeDañoExplosivo = 50;
+    public int armaActual = 0; // 1=Barreta || 2=ArmaScientifica || 3=Explosivo
 
-    public GameObject cuboCerca;
+    //Especificas a Armas Especificas
     private BoxCollider coliderA;
 
     private void Awake() 
     {
         coliderA = transform.Find("Ataque_Collider").gameObject.GetComponent<BoxCollider>();
+          
+    }
+    private void Update() 
+    {
+        if(Input.GetButtonDown("CambiarArma"))
+        {
+            armaActual++;
+            if(armaActual==4)
+            armaActual = 1;
+        }
         
+        if(Input.GetButtonDown("Arma"))
+        {
+            if(armaActual == 2)
+            {
+                UsarArmaScientifica();
+            }
+            else if(armaActual == 3)
+            {
+                TirarExplosivo();
+            }
+        }
     }
     private void OnTriggerStay(Collider other) //Cuando el collider de ataque del jugador choca con otro GameObject
     {
         if(other.tag == "enemigo")              //El GameObject es un enemigo
-        if(Input.GetButtonDown("Arma"))         //USAR BARRETA
+        if(Input.GetButtonDown("Arma") && armaActual==1)         //USAR BARRETA
         {
             other.gameObject.GetComponent<Enemigo_Control>().QuitarVida(porcentajeDañoBarreta);
             PlayAnimacionBarreta();
