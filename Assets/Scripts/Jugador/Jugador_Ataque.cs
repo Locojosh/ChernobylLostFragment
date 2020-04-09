4dd6edbd7340be171b2e3b95750365b2d048b129
Josh <joshuasteele.student@gmail.com>
2020-04-08 23:27:53 -0400

    Programacion- Jugador_Ataque> Acabado.. ataque explosivo aumentado

--- Assets/Scripts/Jugador/Jugador_Ataque.cs
+++ Assets/Scripts/Jugador/Jugador_Ataque.cs
@@ -9,15 +9,29 @@ public class Jugador_Ataque : MonoBehaviour
     public int porcentajeDañoBarreta = 25;
     public int porcentajeDañoArmaScientifica = 15;
     public int porcentajeDañoExplosivo = 50;
-    public int armaActual = 0; // 1=Barreta || 2=ArmaScientifica || 3=Explosivo
-
+    private int armaActual = 0; // 1=Barreta || 2=ArmaScientifica || 3=Explosivo
+    
     //Especificas a Armas Especificas
+    //Barreta
     private BoxCollider coliderA;
+    public string nombreAnimacionBarreta = "barreta"; //Nombre de la animacion de la barreta atacando
+    //Arma scientifica
+    public Rigidbody balaPrefabRB;
+    private GameObject balaPuntoSalida;
+    public int velocidadBala = 50;
+    //Explosivo a lanzar
+    public Rigidbody explosivoPrefabRB; 
+    private GameObject explosivoPuntoSalida;
+    public int velocidadExplosivo = 10;
 
     private void Awake() 
     {
+        //Barreta
         coliderA = transform.Find("Ataque_Collider").gameObject.GetComponent<BoxCollider>();
-          
+        //Arma Scientifica
+        balaPuntoSalida = transform.Find("BalaPuntoSalida").gameObject;
+        //Explosivo a lanzar
+        explosivoPuntoSalida = transform.Find("ExplosivoPuntoSalida").gameObject;
     }
     private void Update() 
     {
@@ -32,7 +46,7 @@ public class Jugador_Ataque : MonoBehaviour
         {
             if(armaActual == 2)
             {
-                UsarArmaScientifica();
+                DispararArmaScientifica();
             }
             else if(armaActual == 3)
             {
@@ -49,18 +63,20 @@ public class Jugador_Ataque : MonoBehaviour
             PlayAnimacionBarreta();
         }         
     }
-    private void UsarArmaScientifica()
+    private void DispararArmaScientifica()
     {
-
+        Rigidbody balaRB = Instantiate(balaPrefabRB, balaPuntoSalida.transform.position, balaPuntoSalida.transform.rotation); //Instancear bala
+        balaRB.velocity = transform.TransformDirection(Vector3.forward * velocidadBala); //Velocidad de la bala
     }
     private void TirarExplosivo()
     {
-
+        Rigidbody explosivoRB = Instantiate(explosivoPrefabRB, explosivoPuntoSalida.transform.position, explosivoPuntoSalida.transform.rotation); //Instancear explosivo a lanzar
+        Vector3 dirExplosivo = new Vector3(0, 0.5f, 1); //Direccion que el explosivo es lanzado
+        explosivoRB.velocity = transform.TransformDirection(dirExplosivo * velocidadExplosivo); //Velocidad del explosivo
     }
-
     private void PlayAnimacionBarreta()
     {
         Animator anim = this.GetComponent<Animator>();  //***NOTA*** El jugador debe tener un componente Animator
-        anim.SetTrigger("barreta");                 //***NOTA*** La animacion de ataque de barreta  del jugador 
-    }                                               //debe tener un trigger llamado "barreta"
+        anim.SetTrigger(nombreAnimacionBarreta);     //La animacion de ataque de barreta  del jugador 
+    }                                              
 }
