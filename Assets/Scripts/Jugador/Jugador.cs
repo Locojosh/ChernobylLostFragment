27f7f51ad798d6831b574410b18643ffd2441a0c
Josh <joshuasteele.student@gmail.com>
2020-04-13 21:08:35 -0400

    Programacion- Jugador > Creacion de script y atributos, barra de vida del UI

---
+++ Assets/Scripts/Jugador/Jugador.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    int vida = 100;
+    string hablar = "Hola";
+    GameObject baulMateriales;
+    string caraActual;
+    public Sprite[] listaCarasUI; 
+    int nivelRadiacion; //Radiacion del lugar en que se encuentra
+
+    public void RecibirDaño(int daño)
+    {
+
+    }
+    public void Hablar(string mensaje)
+    {
+
+    }
+    public void Morir()
+    {
+
+    }
+}
