68e3417879fffcb8e85a55c01eea026a637adbe7
Josh <joshuasteele.student@gmail.com>
2020-04-21 16:22:32 -0400

    Programacion: Enemigo_Control - Propiedades principales

--- Assets/Scripts/Enemigo_Control.cs
+++ Assets/Scripts/Enemigo_Control.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Enemigo_Control : MonoBehaviour
 {
-    public int vida = 100; //hacer private 
+    private int vida = 100; //Cantidad de vida del enemigo
     public int Vida { get{ return vida;} }
+    private int daño = 10; //Cantidad de daño que causa el enemigo con su ataque
+    public int Daño { get { return daño; } set { daño = value; } }
 
 #region Metodos Por Defecto
+    private void Update() 
+    {
+        if(vida <= 0)       //Si vida <= 0, el enemigo muere
+        {
+            Destroy(gameObject);
+        }
+    }
     public void QuitarVida(int valor)
     {
         vida -= valor;
     }
-
 #endregion
 }
