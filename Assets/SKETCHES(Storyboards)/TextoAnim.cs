877be76142be708f7ccd9c17024d4e382cafd824
DanielGuardia <danielmguardia@gmail.com>
2020-05-08 14:18:53 -0400

    ActualizacionDeProyecto

---
+++ Assets/SKETCHES(Storyboards)/TextoAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextoAnim : MonoBehaviour
+{
+    string frase = "El ser Humano es impredesible en sus acciones1111111111111111111111111111111111111111111111111111111111111111111111111111";
+    //string frase2 = "muchas de estas historias";
+    public Text texto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Reloj());
+    }
+
+    IEnumerator Reloj()
+    {
+        foreach (char caracter in frase)
+        {
+            texto.text = texto.text + caracter;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        //foreach (char caracter in frase2)
+        //{
+        //    texto.text = texto.text + caracter;
+        //    yield return new WaitForSeconds(0.1f);
+        //}
+    }
+}
