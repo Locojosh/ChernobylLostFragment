88bc189fff73d1a91f83790fda12a9052aab099f
Josh <joshuasteele.student@gmail.com>
2020-05-29 15:07:58 -0400

    Programacion- MirarAlJugador y Radiacion_Control

---
+++ Assets/MirarAlJugador/MirarAlJugador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirarAlJugador : MonoBehaviour
+{
+    [SerializeField] float distanciaMirar = 100f;
+    private GameObject player;
+    private void Awake() 
+    {
+        player = GameObject.Find("Player");    
+    }
+    private void Update() 
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < distanciaMirar)
+        {
+            transform.LookAt(player.transform);
+            Vector3 eulerPos = transform.localEulerAngles; eulerPos.x = 0; //No mirar en eje x
+            transform.rotation = Quaternion.Euler(eulerPos);  
+        }    
+    }
+}
---
+++ Assets/Radiacion/Radiacion_Control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+public class Radiacion_Control : MonoBehaviour
+{ //Asignar este script a un objeto: 
+  //Cuando el jugador choca contra el objeto se activa el porcentaje de radiacion definido en el objeto
+    [SerializeField]
+    private int porcentajeRadiacion = 2;
+    private Jugador sPlayer;
+    private BoxCollider radiacionCollider;
+    private void Awake() 
+    {
+        radiacionCollider = GetComponent<BoxCollider>();
+
+        sPlayer = GameObject.Find("Player").GetComponent<Jugador>(); 
+    }
+    private void Start() 
+    {
+        radiacionCollider.isTrigger = true;
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        sPlayer.NivelRadiacion = porcentajeRadiacion;
+    }
+}
